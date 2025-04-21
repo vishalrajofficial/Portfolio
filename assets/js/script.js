5c1e72c36cc1be18ef7f08f3511dc7962d13d06c
Vishal Raj <9031842908.vk@gmail.com>
2025-04-21 10:42:51 +0530

    Major changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -126,3 +126,26 @@ function sendMail() {
     })
     .catch();
 }
+
+const soundCloud = document.querySelector('.sound-cloud');
+const off = document.querySelector('#off');
+const on = document.querySelector('#on');
+const myAudio = document.querySelector('#myAudio');
+
+off.addEventListener('click', () => soundTrack('off'));
+on.addEventListener('click', () => soundTrack('on'));
+
+const soundTrack = (soundState) => {
+    if(soundState === 'off'){
+        on.style.display = 'block';
+        off.style.display = 'none';
+        soundCloud.style.color = "#08fdd8";
+        myAudio.play();
+    }
+    else if(soundState === 'on'){
+        on.style.display = 'none';
+        off.style.display = 'block';
+        soundCloud.style.color = "#f50057";
+        myAudio.pause();
+    }
+}
